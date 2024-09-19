a66babf74e08b6233e9e18ecd8e3a3431d76ebfa
your name <thangnguyenxuan2609@gmail.com>
2024-09-19 10:57:12 +0700

    more page

---
+++ src/Pages/product/product.jsx
@@ -0,0 +1,12 @@
+import Slider from "../../components/Slider"
+
+function SanPham () {
+    return (
+        <div className="porduct">
+        <Slider />
+        <h1>Sản phẩm page</h1>
+        </div>
+    )
+}
+
+export default SanPham
